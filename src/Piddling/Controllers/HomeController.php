ee3b4d9ae1363a2ac998f7483d1a517ea922732d
Richard Bagshaw <me@richardbagshaw.co.uk>
2015-02-07 20:22:20 +0000

    Add a dummy controller

---
+++ src/Piddling/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php namespace Piddling\Controllers;
+
+class HomeController {
+
+    public function method1()
+    {
+        die('Method One');
+    }
+
+    public function method2()
+    {
+        die('Method Two');
+    }
+
+}
