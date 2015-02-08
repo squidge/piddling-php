e655f69fbfc4470a9fc05d1d122629d4e5e368a3
Richard Bagshaw <me@richardbagshaw.co.uk>
2015-02-08 23:47:18 +0000

    add dummy controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+class HomeController {
+
+    public function index()
+    {
+        return 'piddling php - pathetically trivial; trifling';
+    }
+
+    public function example()
+    {
+        return 'piddling php - a silly little example route :)';
+    }
+
+}
